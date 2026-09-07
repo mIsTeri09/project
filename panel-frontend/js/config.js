9096f8d5c1dcdd5493d7aa669f93974fe9352cb9
NyonyaTeri09 <muhammadfahmii2409@gmail.com>
2026-09-08 00:34:40 +0700

    Fitur: Menambahkan sistem baru

--- panel-frontend/js/config.js
+++ panel-frontend/js/config.js
@@ -3,7 +3,7 @@
 // ================================================================
 
 // GANTI DENGAN URL BACKEND REPLIT KAMU (tanpa trailing slash)
-export const API_BASE = 'https://nama-repl-mu.username.repl.co';
+export const API_BASE = 'project-production-82c9.up.railway.app';
 
 // WebSocket URL (wss:// untuk secure, atau ws:// jika http)
 export const WS_URL = API_BASE.replace('http', 'ws') + '/socket.io/update';
