a9a0dec6ff4929e6b3c9f375553431bb6390fa91
NyonyaTeri09 <muhammadfahmii2409@gmail.com>
2026-09-07 22:37:32 +0700

    Fitur: Menambahkan sistem baru

--- android-target/app/CryptoHelper.java
+++ android-target/app/CryptoHelper.java
@@ -9,7 +9,7 @@ import java.security.SecureRandom;
 
 public class CryptoHelper {
     private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
-    private static final byte[] KEY = "0123456789abcdef0123456789abcdef".getBytes(); // ganti dengan key dari server
+    private static final byte[] KEY = "10bfc4b9e966985e10688a6756b03c1a".getBytes(); // ganti dengan key dari server
 
     public JSONObject encrypt(JSONObject data) throws Exception {
         SecureRandom random = new SecureRandom();
